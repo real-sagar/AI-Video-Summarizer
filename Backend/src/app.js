b2088558baae0d7b29954fd7493839fe6ee40e88
Sagar Saini <sagarsaini6943@gmail.com>
2026-02-09 18:57:42 +0530

    Change in CORS

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,11 +1,13 @@
 import cors from "cors";
 import express from "express"
 
-const app = express()
 app.use(cors({
-    origin : process.env.CORS_ORIGIN ,
-    credentials : true
-}))
+  origin: [
+    process.env.CORS_ORIGIN, 
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
 
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended : true , limit: "16kb"}))
