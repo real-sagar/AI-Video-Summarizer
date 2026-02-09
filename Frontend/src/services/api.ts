4d88ad5f26e8050ebf58876459066d1430be7c2d
Sagar Saini <sagarsaini6943@gmail.com>
2026-02-10 00:00:18 +0530

    Fix in api fetching 2

--- Frontend/src/services/api.ts
+++ Frontend/src/services/api.ts
@@ -1,15 +1,21 @@
 import axios from "axios";
 
-export const uploadVideo = (formData: FormData) =>
-  axios.post(
+export const uploadVideo = (formData: FormData) => {
+  console.log(
+    "UPLOAD URL 👉",
+    `${import.meta.env.VITE_API_BASE_URL}/upload`
+  );
+
+  return axios.post(
     `${import.meta.env.VITE_API_BASE_URL}/upload`,
     formData,
     {
       headers: { "Content-Type": "multipart/form-data" }
     }
   );
-  console.log("UPLOAD URL →", `${import.meta.env.VITE_API_BASE_URL}/upload`);
+};
 
 
 
-  
+
+
