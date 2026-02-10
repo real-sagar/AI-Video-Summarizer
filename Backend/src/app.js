f2dc5dff51e4bf43a944af0f430c2cbf5b26c9c0
Sagar Saini <sagarsaini6943@gmail.com>
2026-02-10 17:35:31 +0530

    Fix cors settings

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,14 +1,11 @@
 import cors from "cors";
 import express from "express"
 
-const app = express();
+const app = express()
 app.use(cors({
-  origin: [
-    process.env.CORS_ORIGIN, 
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
-}));
+    origin : process.env.CORS_ORIGIN ,
+    credentials : true
+}))
 
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended : true , limit: "16kb"}))
