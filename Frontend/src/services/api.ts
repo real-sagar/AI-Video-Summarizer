4d3db9ae12db048725ffd214080c041386672ba7
Sagar Saini <sagarsaini6943@gmail.com>
2026-02-10 17:52:10 +0530

    Fix app settings

--- Frontend/src/services/api.ts
+++ Frontend/src/services/api.ts
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 export const uploadVideo = (formData: FormData) => {
-  console.log(
-    "UPLOAD URL 👉",
-    `${import.meta.env.VITE_API_BASE_URL}/upload`
-  );
+ 
 
   return axios.post(
     `${import.meta.env.VITE_API_BASE_URL}/upload`,
